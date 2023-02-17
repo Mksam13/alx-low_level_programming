505c6b58cca9b1d1727e51628c2ab4e1427a8e6b
Morris <sammorris431@gmailcom>
2023-02-17 23:38:49 +0000

    using 'puts' to display to screen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(void)
+
+/**
+ * main prints exactly \"Programming is like building a multilingual puzzle
+ * returns (0)
+ */
+
+int main( void )
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
