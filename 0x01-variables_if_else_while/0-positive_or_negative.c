b47df7be6e7c1b4b7b189d6aee5a93d80d698f83
Morris <sammorris431@gmailcom>
2023-02-18 04:43:45 +0000

    using the if statement to print to screen

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Printing n is positive with n is positive number,
+ * n is negative when n is a negative number and 
+ * n is zero when  n is zero.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n<0)
+	{
+		printf("%d is negative\n",n);
+	}
+	else if (n==0)
+	{
+		printf("%d is 0\n",n);
+	}
+	return (0);
+}
