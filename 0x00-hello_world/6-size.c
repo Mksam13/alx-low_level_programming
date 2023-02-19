90403936181ea1b79859f429369577c1c612a832
Morris <sammorris431@gmailcom>
2023-02-19 13:49:32 +0000

    using 'sizeof' to print the size of the data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing to screen the various data type sizes of my laptop which is a 64-bit laptop
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
