9b444e27f41a8eaba2241b186380e140d9d31636
Morris <sammorris431@gmailcom>
2023-02-19 15:44:21 +0000

    printing to screen in a vertical order a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabets in Lower case
+ *
+ * Return: 0 Always (Succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+}
