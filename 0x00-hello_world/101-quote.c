dd065eba70823fdac0fb09eb53c963f422bf58d4
Morris <sammorris431@gmail.com>
2023-02-19 17:20:01 -0800

    using a diffent library aside stdio.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing a sentence to the standard error
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
