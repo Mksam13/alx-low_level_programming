4974adf88f2342a23e272bf6612c23f425ae38f9
Morris <sammorris431@gmailcom>
2023-02-22 14:34:09 +0000

    creating header files, custom fuctions in differenct files and linking them

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Printing putchar to screen
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
