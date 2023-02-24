4e6df27fd40dea4d5dc9b0414970cb0b7ac9df4b
Morris <sammorris431@gmailcom>
2023-02-24 12:20:30 +0000

    checking upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,10 +8,11 @@
 
 int _isupper(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		_putchar(0 + '0');
-	else if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		_putchar(1 + '0');
+	else
+		_putchar(0 + '0');
+
 	_putchar('\n');
 	return (0);
 }
