de2f8556605b599983b5d5be5f3e3c8917330cee
Morris <sammorris431@gmailcom>
2023-03-03 04:02:26 +0000

    converting lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - converting lowercase strings to upper case
+ * @n: characters to check
+ *
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i - 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (0);
+}
